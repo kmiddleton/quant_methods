ae2fdbe597409ed59d38ffc61a7fcdfdab623cde
Kevin Middleton <middletonk@missouri.edu>
2021-02-05 16:00:58 -0600

    Update Quiz_03-3.R

--- Quiz_Code/Quiz_03-3.R
+++ Quiz_Code/Quiz_03-3.R
@@ -8,7 +8,7 @@ b1 <- beaver1 %>%
 
 air_temps <- tibble(
   day = 346,
-  time = as.integer(c(830, 835, 840, 845, 850, 855,
+  time = as.integer(ca(830, 835, 840, 845, 850, 855,
                       900, 905, 910, 915, 920, 930, 935, 940, 945, 950, 955,
                       1000, 1005, 1010, 1015, 1020, 1025)),
   air_temp = runif(length(time), 9, 10)
@@ -19,3 +19,6 @@ air_temps
 
 left_join(b1, air_temps)
 full_join(b1, air_temps)
+
+left_join(b1 %>% select(-time),
+          air_temps %>% select(-time))
